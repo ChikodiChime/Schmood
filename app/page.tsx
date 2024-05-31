edca6dce480f68ba6a7c9cf90a5f66b9aee88774
--global <chikkychime@gmail.com>
2024-05-31 13:16:05 +0100

    Moods Added

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ const Home = () => {
             height={800}
             />
           </div>
-          <div className="w-1/2 text-center space-y-3 flex flex-col items-center jusc'">
-            <h1 className='salsa-regular text-6xl font-black text-[#E3694C]'>Can&apos;t figure out what to eat?</h1>
+          <div className="w-1/2 text-center space-y-3 flex flex-col items-center ">
+            <h1 className='salsa-regular '>Can&apos;t figure out what to eat?</h1>
             <p className='text-xl '>We gatchu covered boo!</p>
             <a href='/Moods'  className='px-12 py-3 bg-[#E3694C] text-white flex items-center justify-center gap-2 cursor-pointer'>Next <FaArrowRight/></a>
           </div>
